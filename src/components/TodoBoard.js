812b9e596b27bcced5a04e50246b39b491c2ffc0
NEZ\DESKTOP <elsword485667@gmail.com>
2024-05-29 01:14:10 +0900

    1.0.0

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,21 +1,24 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
-  return (
-    <div>
-      <h2>Todo List</h2>
-      {todoList.length > 0 &&
-        todoList.map((item, index) => (
-          <TodoItem
-            item={item}
-            key={index}
-            deleteItem={deleteItem}
-            toggleComplete={toggleComplete}
-          />
-        ))}
-    </div>
-  );
+const TodoBoard = ({ todoList = [], deleteItem, toggleComplete }) => {
+    return (
+        <div>
+            <h2>Todo List</h2>
+            {todoList.length > 0 ? (
+                todoList.map((item, index) => (
+                    <TodoItem
+                        item={item}
+                        key={index}
+                        deleteItem={deleteItem}
+                        toggleComplete={toggleComplete}
+                    />
+                ))
+            ) : (
+                <p>No todos available</p>
+            )}
+        </div>
+    );
 };
 
 export default TodoBoard;
